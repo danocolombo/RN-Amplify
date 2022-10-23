98425ce3db87f97731a8190b38a74418eee48cb2
Dano Colombo <danocolombo@gmail.com>
2022-10-22 20:46:41 -0400

    auth done, working on data now

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
-import Navigator from "./src/navigation";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import Navigator from './src/navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <SafeAreaProvider style={styles.container}>
-      <Navigator />
+import { Amplify, Auth, DataStore } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+function App() {
+    // Auth.currentAuthenticatedUser().then((data) => console.log(data));
+    return (
+        <SafeAreaProvider style={styles.container}>
+            <Navigator />
 
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
-  );
+            <StatusBar style='auto' />
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#c9c9c9",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#c9c9c9',
+    },
 });
+export default withAuthenticator(App);
